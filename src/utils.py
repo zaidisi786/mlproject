d6c54df2e976fd6b4fc2d60aea2849c6d49e0a34
zaidisi786 <syed.ijlal.zaidi@gmail.com>
2025-11-27 17:10:46 -0600

    Model trainer

--- src/utils.py
+++ src/utils.py
@@ -3,9 +3,11 @@ import os
 import sys
 import numpy as np
 import pandas as pd
-import dill
+import dill  # library to help crete pkl filr
+from sklearn.metrics import r2_score
 
 sys.path.append('C:\\Projects\\mlproject')
+
 
 from src.exception import CustomException
 
@@ -21,4 +23,27 @@ def save_object(file_path, obj):
             
     except Exception as e:
         raise CustomException(e, sys)
-            
+
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+
+        for name, model in models.items():
+            # train the model
+            model.fit(X_train, y_train)
+
+            # predict the train and test data
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            # train, test model scores
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[name] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)          
+        
